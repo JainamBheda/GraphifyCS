688deb363ada1e412b0f6637dd77096a306c282a
Jainam Bheda <jainambheda@gmail.com>
2025-08-22 10:15:24 +0530

    Auto-commit at Fri Aug 22 10:15:24 IST 2025 by Jainam Bheda

--- routes/cnRoutes.js
+++ routes/cnRoutes.js
@@ -5,4 +5,5 @@ const CNController = require('../controllers/cnController');
 // ARP Simulation Page
 router.get('/arp', CNController.getARPPage);
 router.get('/routing', CNController.getRoutingPage);
+router.get('/tcpip', CNController.getTCPIPPage);
 module.exports = router;
